075ee33a94fad34aeb12a6b2068d7a8e851abf62
Danimagnum <135241520+Danimagnum@users.noreply.github.com>
2026-08-14 16:38:34 +0200

    Add PrivacyPolicyPage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ScrollToTop } from "@/components/ScrollToTop";
 import { HomePage } from "@/pages/HomePage";
 import { NotFoundPage } from "@/pages/NotFoundPage";
+import { PrivacyPolicyPage } from "@/pages/PrivacyPolicyPage";
 import { ServicePage } from "@/pages/ServicePage";
 
 export default function App() {
@@ -12,6 +13,12 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/servicios/:slug" element={<ServicePage />} />
+
+        <Route
+          path="/politica-de-privacidad"
+          element={<PrivacyPolicyPage />}
+        />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
